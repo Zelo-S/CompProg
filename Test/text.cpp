32817fab7362d9ef52a08d65a9a7d833c28b1082
Steve Xing <stevethesteve990@gmail.com>
2022-05-25 20:53:40 -0400

    Update Settings

--- Test/text.cpp
+++ Test/text.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+    
 int main(){
     cout<<1<<endl;
+    vector<int> arr;
+    arr.push_back(1);
+    map<int, int> np;
+     
+     
     return 0;
 }
