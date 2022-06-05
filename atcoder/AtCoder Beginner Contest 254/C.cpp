2efba0017ee117bd6d81994e9310b649c708e759
Steve Xing <stevethesteve990@gmail.com>
2022-06-05 19:44:49 -0400

    atcoder 254 C Solved, UPSOLVE

--- atcoder/AtCoder Beginner Contest 254/C.cpp
+++ atcoder/AtCoder Beginner Contest 254/C.cpp
@@ -1,12 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+
 void solve(){
-    int N,K; cin>>N>>K;
-    vector<int> arr(N);
-    for(auto& f : arr) cin>>f;
+    ll N, K; cin>>N>>K;
+    vector<ll> arr(N);
+    for(auto& f: arr) cin>>f;
+
+    vector<ll> sorted = arr;
+    sort(sorted.begin(), sorted.end());
     
+    for(ll link_n=0; link_n<K; ++link_n){
+        map<ll, ll> run_uns;
+        for(ll ptr_uns=link_n; ptr_uns<N; ptr_uns+=K) { // ptr_uns = pointer for unsorted array(arr)
+            ++run_uns[arr[ptr_uns]];
+        }
+
+        map<ll, ll> run_sr;
+        for(ll ptr_sr=link_n; ptr_sr<N; ptr_sr+=K) { // ptr_sr = pointer for sorted array(sorted)
+            ++run_sr[sorted[ptr_sr]];
+        }
+        
+        for(auto [f, s] : run_uns){
+            if(run_sr[f] != s){
+                cout<<"No\n";
+                return;
+            }
+        }
+    }
     
+    cout<<"Yes\n";
     
 }
 
