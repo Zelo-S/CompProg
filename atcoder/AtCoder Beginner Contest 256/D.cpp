5473ac21b62520e64dee46d0ddad8c1cf912a351
Steve Xing <stevethesteve990@gmail.com>
2022-06-18 10:45:31 -0400

    AtCoder 256D Solved, Upsolve

--- atcoder/AtCoder Beginner Contest 256/D.cpp
+++ atcoder/AtCoder Beginner Contest 256/D.cpp
@@ -19,14 +19,19 @@ void solve(){
     
     sort(arr.begin(), arr.end(), cmp);
     
-    int startptr = 0, endptr = startptr+1;
-    while(startptr < N || endptr <= N) {
-        if(arr[endptr].f > arr[endptr-1].s){
-            cout<<arr[startptr].f<<" "<<arr[endptr-1].s<<endl;
-            startptr = endptr;
+    int earliest = arr[0].f, latest = arr[0].s, ptr = 0;
+    while(ptr <= N) {
+        if(latest >= arr[ptr].f) {
+            // continue a chain, so keep on updating latest's position
+            latest = max(latest, arr[ptr].s);
+        }else if(latest < arr[ptr].f){ // we can break a chain now
+            cout<<earliest<<" "<<latest<<endl;
+            earliest = arr[ptr].f, latest = arr[ptr].s; // update earliest and latest to ptr's spot. We are now starting a new chain
         }
-        ++endptr; 
+        ++ptr; // advance ptr
     }
+    
+    // ok i know I could've used a for loop for this but whatever, it makes my code look more complex
 
 }
 
