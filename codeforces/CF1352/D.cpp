b6876eae9ebed2d0d0cd62ae29ca241e4d0873cd
Steve Xing <stevethesteve990@gmail.com>
2022-06-22 12:15:15 -0400

    CF1352 D Tried again

--- codeforces/CF1352/D.cpp
+++ codeforces/CF1352/D.cpp
@@ -7,33 +7,39 @@ void solve(){
         int N; cin>>N;
         vector<int> arr(N);
         for(auto& f : arr) cin>>f;
+        vector<int> taken(N);
         
+        /*
         vector<int> prefix(N+1), suffix(N+1);
         for(int i=1; i<=N; ++i) prefix[i] = prefix[i-1] + arr[i-1];
         for(int i=N-1; i>=0; --i) suffix[i] = suffix[i+1] + arr[i];
+        */
         
-        int A_ptr = 0, B_ptr = N;
+        int A_ptr = 0, B_ptr = N-1;
         int A_count = 0, B_count = 0;
         int move_count = 0;
         
         bool isATurn = true;
 
+        int AprevAdd = 0, BprevAdd = 0;
         while(A_ptr < B_ptr){
             if(isATurn){
-                while(prefix[A_ptr] - A_count <= suffix[B_ptr] - B_count && A_ptr<B_ptr){
-                    ++A_ptr; 
+                AprevAdd = 0;
+                while(AprevAdd <= BprevAdd && !taken[A_ptr]) {
+                    AprevAdd += arr[A_ptr];
+                    taken[A_ptr] = 1;
+                    ++A_ptr;
                 }
-                A_count = prefix[A_ptr];
+                A_count += AprevAdd;
             }else{
-                while(suffix[B_ptr] - B_count <= prefix[A_ptr] - A_count && A_ptr<B_ptr){
+                BprevAdd = 0;
+                while(BprevAdd <= AprevAdd && !taken[B_ptr]) {
+                    BprevAdd += arr[B_ptr];
+                    taken[B_ptr] = 1;
                     --B_ptr;
                 }
-                B_count = suffix[B_ptr];
+                B_count += BprevAdd;
             }
-            
-            if(isATurn) cout<<"A: ";
-            else cout<<"B: ";
-            cout<<A_ptr<<": "<<A_count<<" "<<B_ptr<<": "<<B_count<<endl;
             
             ++move_count;
             isATurn = !isATurn;
