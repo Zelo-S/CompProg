3a41142d23d94460d230657208b44049d93908cc
Steve Xing <stevethesteve990@gmail.com>
2022-06-26 23:39:13 -0400

    CF1333 B AC

--- codeforces/CF1333/B.cpp
+++ codeforces/CF1333/B.cpp
@@ -1,45 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long LL;
+struct State{
+    bool has1;
+    bool hasn1;
+    bool has0;
+};
 
 void solve(){
-    LL T; cin>>T;
+    int T; cin>>T;
     while(T--){
-        LL N; cin>>N;
-        vector<LL> a(N), b(N);
+        int N; cin>>N;
+        vector<int> a(N), b(N);
         for(auto& f : a) cin>>f;
         for(auto& f : b) cin>>f;
         
-        bool valid = a[0] == b[0];
-
-        // 0 == 0, -1 == 1, 1 == 2, -1 and 1 == 3
-        for(auto& f : a){ if(f == 0) f = 0; else if(f == -1) f = 1; else if(f == 1) f = 2; }
-
-        vector<LL> fourStatePrefix(N+1);
-        for(LL i=1; i<=N; ++i){
-            if(fourStatePrefix[i-1] == a[i-1]) { fourStatePrefix[i] = fourStatePrefix[i-1]; continue; }
-            fourStatePrefix[i] = fourStatePrefix[i-1] + a[i-1];
-            if(fourStatePrefix[i] > 3) fourStatePrefix[i] = 3;
+        vector<int> diff(N);
+        for(int i=0; i<N; ++i){
+            diff[i] = b[i] - a[i];
         }
         
-        for(LL i=1; i<N; ++i){ // iterating thru b
-            if(fourStatePrefix[i] == 3) continue;
-            if(b[i] > 0){
-                if(fourStatePrefix[i] != 2) valid = false;
-            }else if(b[i] < 0){
-                if(fourStatePrefix[i] != 1) valid = false;
-            }else if(b[i] == 0){
-                if(fourStatePrefix[i] != 0) valid = false;
+        
+        vector<State> states(N+1);
+        for(int i=1; i<N; ++i){
+            states[i].has1 = states[i-1].has1;
+            states[i].hasn1 = states[i-1].hasn1;
+            states[i].has0 = states[i-1].has0;
+            if(a[i-1] == 1) states[i].has1 = true;
+            if(a[i-1] == -1) states[i].hasn1 = true;
+            if(a[i-1] == 0) states[i].has0 = true;
+        }
+        
+        bool valid = true;
+        for(int i=0; i<N; ++i){
+            // if diff[i] == 0, we dont need any operations
+            if(diff[i] > 0){
+                if(!states[i].has1) valid = false;
+            }else if(diff[i] < 0){
+                if(!states[i].hasn1) valid = false;
             }
         }
         
-        // for(auto f : fourStatePrefix) cout<<f<<" "; cout<<endl;
-        
         if(valid) cout<<"YES\n";
         else cout<<"NO\n";
+        
+    }
 
-    } 
 }
 
 int main(){
